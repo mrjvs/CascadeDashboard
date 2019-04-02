13e1c5cf6283130e0c758213adb7400231cd10d4
mrjvs <jellevs@gmail.com>
2019-04-02 21:43:31 +0200

    fixed path issue

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -4,13 +4,13 @@ import path from "path";
 
 const dashboardRouter: Router = Router();
 
-const dashboardRoot: string = path.join(__dirname, "client/dist");
+const dashboardRoot: string = path.join(__dirname, "../../client/dist");
 
 // serve static dashboard files
 dashboardRouter.use(express.static(dashboardRoot));
 
 // all unknown routes go to index.html for vue.js to handle
-dashboardRouter.use((req: Request, res: Response, next: NextFunction) => {
+dashboardRouter.use((req: Request, res: Response, next: NextFunction): void => {
     return res.sendFile("index.html", {
         root: dashboardRoot
     });
