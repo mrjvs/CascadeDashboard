8af904c001b2c3f31587dc8d03c027bc968d6b4c
William Oldham <wegg7250@gmail.com>
2019-04-02 21:44:38 +0100

    Oops this was bitwise not logic

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import dashboardRouter from "./routes/dashboard";
 const app: express.Application = express();
 app.use(dashboardRouter);
 
-const port : number = Number(process.env.PORT) | 8080;
+const port : number = Number(process.env.PORT) || 8080;
 
 app.listen(port);
 console.log(`Running on port ${port}`);
