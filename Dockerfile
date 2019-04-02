34d24c0b3a29cf7185605b719356b534d86bd745
William Oldham <wegg7250@gmail.com>
2019-04-02 21:18:22 +0100

    All the docker stuff :)))

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM node:lts-alpine
+
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+COPY client/package*.json ./client/
+
+RUN npm install
+RUN cd client && npm install
+
+COPY . .
+
+RUN npm run build
+
+EXPOSE 8080
+ENV PORT 8080
+
+CMD ["npm", "run", "prod"]
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import express from "express";
 
-// configuration
-import { port } from "./config.json";
-
 // local imports
 import dashboardRouter from "./routes/dashboard";
 
 // express setup
 const app: express.Application = express();
 app.use(dashboardRouter);
+
+const port : number = Number(process.env.PORT) | 8080;
 
 app.listen(port);
 console.log(`Running on port ${port}`);
