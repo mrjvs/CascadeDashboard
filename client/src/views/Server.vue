16e0ef4ff8a68df17a6b748c6f5f2af1a806f37f
mrjvs <jellevs@gmail.com>
2019-08-07 17:36:01 +0200

    error/empty/loading states for queries

--- client/src/views/Server.vue
+++ client/src/views/Server.vue
@@ -1,32 +1,53 @@
 <template>
-    <div class="server">
-        <ApolloQuery
-        :query="require('../graphql/guild.gql')"
-        :variables="{ id: $route.params.id }"
-        >
-        <template slot-scope="{ result: { loading, error, data } }">
-            <loading v-if="loading"/>
-            <div v-else-if="error">An error occured. Please try again later</div>
-            <div v-else-if="data">
-                <h1>Server view</h1>
-                <div class="nav">
-                    <router-link to="general">General</router-link><br>
-                    <router-link to="modlog">Modlog</router-link>
-                </div>
-                <router-view name="settings" :guild="data.guild"/>
-            </div>
-            <div v-else>Invalid url</div>
-        </template>
-        </ApolloQuery>
+  <div class="server">
+    <Loading v-if="$apollo.loading"/>
+    <div v-else-if="error">An error occured. Please try again later</div>
+    <div v-else-if="guild && guild.empty !== true">
+      <h1>Server view</h1>
+      <div class="nav">
+        <router-link to="general">General</router-link><br>
+        <router-link to="modlog">Modlog</router-link>
+      </div>
+      <router-view name="settings" :guild="guild"/>
     </div>
+    <div v-else>Not found</div>
+  </div>
 </template>
 
 <script>
+import gql from 'graphql-tag';
 import Loading from '@/components/Loading.vue';
 
 export default {
   components: {
     Loading,
+  },
+  data() {
+    return {
+      guild: {
+        empty: true,
+      },
+      error: null,
+    };
+  },
+  apollo: {
+    guild: {
+      query: gql`
+        query guild($id: Long!) {
+          guild(id: $id) {
+            memberCount
+          }
+        }
+      `,
+      variables() {
+        return {
+          id: this.$route.params.id,
+        };
+      },
+      error(error) {
+        this.error = error;
+      },
+    },
   },
 };
 </script>
--- client/src/views/ServerList.vue
+++ client/src/views/ServerList.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="serverlist">
         <router-link to="/488394590458478602">Cascade's official server</router-link><br>
+        <router-link to="/12345">unauthorized guild test</router-link><br>
         <router-link to="/abcdefg">invalid link test</router-link>
     </div>
 </template>
