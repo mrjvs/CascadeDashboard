9d1a6f1bcb9e4f14968a35c1af41ccf24f228ed7
mrjvs <jellevs@gmail.com>
2019-08-12 16:45:06 +0200

    update signature generation

--- src/utils/signature.ts
+++ src/utils/signature.ts
@@ -1,40 +1,28 @@
 import crypto from 'crypto';
 
 // configuration
-const config = require('../config.json');
+import config from '../config';
 const { signatureSecret, signatureTimeframe, signatureAlgorithm } = config.signature;
 
-export function getTimedSecret(time: number = new Date().getTime()):
-    {secret: string, time: number} {
-    const hash: string = crypto.createHmac(signatureAlgorithm, signatureSecret )
-        .update(time.toString())
-        .digest('hex');
-    return {
-        secret: hash,
-        time,
-    };
-}
-
-export function getSignature(userID: string, timedSecret: any = getTimedSecret()): {
+export function getSignature(userID: string, time: number = new Date().getTime()): {
     signature: string,
-    timeframe: number,
-    creation: number,
+    expiry: number,
     userID: string,
 } {
-    const finalSignature: string = crypto.createHmac(signatureAlgorithm, timedSecret.secret)
+    const finalSignature: string = crypto.createHmac(signatureAlgorithm, signatureSecret)
+        .update(time.toString())
         .update(userID)
         .digest('hex');
     return {
         signature: finalSignature,
-        timeframe: signatureTimeframe,
-        creation: timedSecret.time,
+        expiry: time + signatureTimeframe,
         userID,
     };
 }
 
-export function isSignatureValid(signature: string, creationDate: number, userID: string): boolean {
-    if (creationDate + signatureTimeframe <= new Date().getTime()) {
+export function isSignatureValid(signature: string, expiry: number, userID: string): boolean {
+    if (expiry <= new Date().getTime()) {
         return false;
     }
-    return signature === getSignature(userID, getTimedSecret(creationDate)).signature;
+    return signature === getSignature(userID).signature;
 }
