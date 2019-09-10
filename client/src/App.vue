8ed354bb6448d5396c39c35d0fa12e13bc0f3f04
mrjvs <jellevs@gmail.com>
2019-09-10 15:31:03 +0200

    Improved body styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-    <div id="app">
-        <router-view v-if="isAuthed === true"/>
-        <Login v-else-if="isAuthed === false"/>
-        <Loading v-else/>
-    </div>
+  <div id="app">
+    <router-view v-if="isAuthed === true" />
+    <Login v-else-if="isAuthed === false" />
+    <Loading v-else />
+  </div>
 </template>
 
 <script>
@@ -23,34 +23,32 @@ export default {
   },
   created() {
     // do auth check
-    isAuthenticated().then((res) => {
-      this.isAuthed = res;
-      this.$store.commit('setLoading', false);
-    }).catch(() => {
-      this.isAuthed = false;
-      this.$store.commit('setLoading', false);
-    });
+    isAuthenticated()
+      .then((res) => {
+        this.isAuthed = res;
+        this.$store.commit('setLoading', false);
+      })
+      .catch(() => {
+        this.isAuthed = false;
+        this.$store.commit('setLoading', false);
+      });
   },
 };
 </script>
 
-
 <style lang="scss">
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #181836;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
 }
-#nav {
-    padding: 30px;
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
+body {
+  margin: 0;
+  padding: 0;
+  font-size: 1em;
 }
 </style>
