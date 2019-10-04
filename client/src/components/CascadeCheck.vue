9e86046e60761a020351357c0d07800f208b41aa
mrjvs <jellevs@gmail.com>
2019-10-04 15:10:31 +0200

    added toggle

--- client/src/components/CascadeCheck.vue
+++ client/src/components/CascadeCheck.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="cascade-check">
     <label>{{ this.$props.label }}</label>
-    <input type="checkbox" v-model="dataModel">
+    <div v-on:click="toggle" :class="dataModel ? 'toggle' : 'toggle on'"></div>
+    <input type="checkbox" v-model="dataModel" />
   </div>
 </template>
 
@@ -28,6 +29,11 @@ export default {
       },
     },
   },
+  methods: {
+    toggle() {
+      this.dataModel = !this.dataModel;
+    },
+  },
 };
 </script>
 
@@ -35,10 +41,57 @@ export default {
   .cascade-check {
     display: flex;
     margin: 1.5rem 0 0 0;
+    align-items: center;
 
     label {
       flex: 1;
       color: white;
+    }
+
+    input {
+      display: none;
+    }
+
+    .toggle {
+      display: block;
+      width: 3rem;
+      height: 1.5rem;
+      background-color: #36365F;
+      border-radius: 1.5rem;
+      position: relative;
+
+      &:hover {
+        background-color: #2b2b50;
+      }
+
+      &:active:before {
+        opacity: .75;
+      }
+
+      &:before {
+        content: "";
+        display: block;
+        height: calc(1.5rem - .4rem);
+        width: calc(1.5rem - .4rem);
+        box-sizing: border-box;
+        top: .2rem;
+        left: .2rem;
+        position: absolute;
+        background-color: white;
+        border-radius: 50%;
+        transition: left .1s ease-out;
+      }
+      &.on {
+        background-color: #24B69E;
+
+        &:before {
+          left: 1.7rem;
+        }
+
+        &:hover {
+          background-color: #189782;
+        }
+      }
     }
   }
 </style>
